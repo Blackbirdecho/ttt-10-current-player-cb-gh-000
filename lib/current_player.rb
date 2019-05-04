c3f612f628427fba98423c1659be24c05992bcbe
Khaled Ali Ahmed <s.echo.bird@gmail.com>
2019-05-04 16:27:34 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,19 @@
-counter = 0
-if my_condition_is_met
-  counter = counter + 1
+def turn_count(board)
+  counter = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
+  counter
 end
+
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+ 
